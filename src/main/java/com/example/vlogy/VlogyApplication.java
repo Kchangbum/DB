b3ed85f403bace0b3da7a525158bf11acb83da64
bum <pc7537@naver.com>
2023-09-25 18:48:26 +0900

    test

---
+++ src/main/java/com/example/vlogy/VlogyApplication.java
@@ -0,0 +1,13 @@
+package com.example.vlogy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VlogyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VlogyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vlogy/VlogyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vlogy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VlogyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
